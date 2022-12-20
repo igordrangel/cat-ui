f323f0d85400ed2a896af7834d26e44eb37f4a76
Igor Rangel <igordrangel@gmail.com>
2022-12-19 22:22:34 -0300

    ajustes

--- projects/cat-ui/src/app/pages/components/form/page-form.component.ts
+++ projects/cat-ui/src/app/pages/components/form/page-form.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
+import { CatFileInterface } from '@catrx/ui/core';
 import {
-  CatFileInterface,
   CatFormBehavior,
   CatFormService,
 } from '@catrx/ui/form';
