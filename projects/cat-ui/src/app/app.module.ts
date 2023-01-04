deabda18afb81c5ebf8a90a04c9bc57ff1756978
Igor Rangel <igordrangel@gmail.com>
2023-01-04 11:41:59 -0300

    implementando xlsx

--- projects/cat-ui/src/app/app.module.ts
+++ projects/cat-ui/src/app/app.module.ts
@@ -7,7 +7,13 @@ import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [CatUiModule.forRoot(environment), AppRoutingModule],
+  imports: [CatUiModule.forRoot(environment, {
+    xlsxConfig: {
+      headerBackgroundColor: '#212121',
+      headerFontColor: '#f1f1f1',
+      normalizeHeader: true
+    }
+  }), AppRoutingModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ projects/utils/src/lib/xlsx/cat-xlsx.service.ts
@@ -0,0 +1,70 @@
+import * as Excel from 'exceljs/dist/exceljs.min.js';
+import * as ExcelProper from 'exceljs';
+import * as fs from 'file-saver';
+
+import { koala } from '@koalarx/utils';
+import { Injectable } from '@angular/core';
+import { CatXlsxConfig } from './xlsx.config';
+
+@Injectable({ providedIn: 'any' })
+export class CatXlsxService {
+  public async convertJsonToXlsx(
+    tabs: {
+      sheetName: string;
+      json: any[];
+    }[],
+    filename: string
+  ) {
+    const config = CatXlsxConfig.config;
+    const workbook: ExcelProper.Workbook = new Excel.Workbook();
+
+    tabs.forEach((tab) => {
+      const header = [];
+      Object.keys(tab.json[0] ?? {}).forEach((name) => {
+        header.push(
+          config.normalizeHeader === true ||
+            config.normalizeHeader === null ||
+            config.normalizeHeader === undefined
+            ? koala(name).string().normalize().getValue().toUpperCase()
+            : name
+        );
+      });
+
+      if (header.length > 0) {
+        const worksheet = workbook.addWorksheet(
+          tab.sheetName
+        );
+
+        const headerRow = worksheet.addRow(header);
+        headerRow.eachCell((cell, number) => {
+          if (config.headerBackgroundColor) {
+            cell.fill = {
+              type: 'pattern',
+              pattern: 'solid',
+              fgColor: { argb: config.headerBackgroundColor.replace('#', '') },
+              bgColor: { argb: config.headerBackgroundColor.replace('#', '') },
+            };
+          }
+          if (config.headerFontColor) {
+            cell.font = {
+              color: { argb: config.headerFontColor.replace('#', '') },
+            };
+          }
+        });
+
+        tab.json.forEach((item) => {
+          const data = [];
+          Object.values(item).forEach((value) => data.push(value));
+          worksheet.addRow(data);
+        });
+      }
+    });
+
+    workbook.xlsx.writeBuffer().then((data) => {
+      const blob = new Blob([data], {
+        type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      });
+      fs.saveAs(blob, filename + '.xlsx');
+    });
+  }
+}
