fa0a5f684054f1bf8a621596e89556f0b697ab61
Igor Rangel <igordrangel@gmail.com>
2023-01-24 21:45:26 -0300

    ajuste tema

--- projects/cat-ui/src/app/pages/crud-example/page-crud-example.routing.module.ts
+++ projects/cat-ui/src/app/pages/crud-example/page-crud-example.routing.module.ts
@@ -1,13 +1,11 @@
 import { Routes, RouterModule } from '@angular/router';
 import { PageCRUDExampleComponent } from './page-crud-example.component';
 import { NgModule } from '@angular/core';
-import { CatAuthGuard } from '@catrx/ui/core';
 
 const routes: Routes = [
   {
     path: '',
     component: PageCRUDExampleComponent,
-    canActivate: [CatAuthGuard],
   },
 ];
 
