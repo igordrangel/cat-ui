3739b48f965b18c3ce790b96573f0e7f39b35455
Igor Rangel <igordrangel@gmail.com>
2023-04-18 20:04:30 -0300

    bugfix csvService Papa provider

--- projects/catrx/ui/utils/csv/cat-csv.service.ts
+++ projects/catrx/ui/utils/csv/cat-csv.service.ts
@@ -3,7 +3,7 @@ import { Papa } from 'ngx-papaparse';
 import { UnparseData } from 'ngx-papaparse/lib/interfaces/unparse-data';
 import { CatFileInterface, CatFileService } from '@catrx/ui/utils';
 
-@Injectable({ providedIn: 'any' })
+@Injectable({ providedIn: 'any', deps: [Papa] })
 export class CatCsvService {
   constructor(private papa: Papa, private fileService: CatFileService) {}
 
