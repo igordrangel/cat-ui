b920088962d69ab0c4bb0cd63bfa81dfd988271b
Igor Rangel <igordrangel@gmail.com>
2023-05-04 16:09:01 -0300

    ng update

--- projects/catrx/ui/src/core/guard/cat-auth.guard.ts
+++ projects/catrx/ui/src/core/guard/cat-auth.guard.ts
@@ -1,16 +1,11 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  RouterStateSnapshot,
-  UrlTree,
-} from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs/internal/Observable';
 import { TokenFactory } from '../factory/token.factory';
 import { CatRoutePolice } from './cat-route.police';
 
 @Injectable()
-export class CatAuthGuard implements CanActivate {
+export class CatAuthGuard  {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
