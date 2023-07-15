d871fccd71363e49a3c26f9dafb2654cf1e1f8a8
Igor Rangel <igordrangel@gmail.com>
2023-07-15 10:49:56 -0300

    ajustes

--- projects/catrx/ui/datatable/src/datatable.component.ts
+++ projects/catrx/ui/datatable/src/datatable.component.ts
@@ -96,59 +96,58 @@ export class DatatableComponent implements OnInit, OnDestroy {
       (this.config.filter$ ?? new BehaviorSubject(null))
         .pipe(takeUntil(this.destroySubscriptions$))
         .subscribe((filter) => {
-          if (this.config?.typeDataList === 'all') {
-            if (this.datatableBackupList$.getValue().length === 0) {
-              this.loadData();
-            } else {
-              if (typeof filter === 'string') {
-                this.datatableList$.next(
-                  Ng2SearchPipe.filter(this.datatableBackupList$.getValue(), filter)
-                );
-              } else if (typeof filter === 'object') {
-                this.objectFilter = filter;
+          if (typeof filter === 'string' && this.config?.typeDataList === 'all') {
+            this.datatableList$.next(
+              Ng2SearchPipe.filter(this.datatableBackupList$.getValue(), filter)
+            );
+          } else if (typeof filter === 'object') {
+            this.objectFilter = filter;
 
-                let filteredList = this.datatableBackupList$.getValue();
-                filteredList = filteredList.filter((itemLine) => {
-                  const dataFiltered = [];
-                  Object.keys(filter).forEach((indexNameFilter) => {
-                    if (filter[indexNameFilter] && indexNameFilter !== 'filter') {
-                      if (
-                        typeof itemLine[indexNameFilter] === 'string' &&
-                        typeof filter[indexNameFilter] === 'string'
-                      ) {
-                        dataFiltered.push(
-                          (itemLine[indexNameFilter] as string)
-                            .toLowerCase()
-                            .includes(
-                              (filter[indexNameFilter] as string).toLowerCase()
-                            )
-                        );
-                      } else {
-                        dataFiltered.push(
-                          JSON.stringify(itemLine[indexNameFilter]) ===
-                          JSON.stringify(filter[indexNameFilter]) ||
-                          (itemLine[indexNameFilter]?.['id'] ?? 0) ===
-                          (filter[indexNameFilter]?.['id'] ?? -1) ||
-                          (itemLine[indexNameFilter]?.['codigo'] ?? 0) ===
-                          (filter[indexNameFilter]?.['codigo'] ?? -1)
-                        );
-                      }
+            if (this.config?.typeDataList === 'all') {
+              let filteredList = this.datatableBackupList$.getValue();
+              filteredList = filteredList.filter((itemLine) => {
+                const dataFiltered = [];
+                Object.keys(filter).forEach((indexNameFilter) => {
+                  if (filter[indexNameFilter] && indexNameFilter !== 'filter') {
+                    if (
+                      typeof itemLine[indexNameFilter] === 'string' &&
+                      typeof filter[indexNameFilter] === 'string'
+                    ) {
+                      dataFiltered.push(
+                        (itemLine[indexNameFilter] as string)
+                          .toLowerCase()
+                          .includes(
+                            (filter[indexNameFilter] as string).toLowerCase()
+                          )
+                      );
+                    } else {
+                      dataFiltered.push(
+                        JSON.stringify(itemLine[indexNameFilter]) ===
+                        JSON.stringify(filter[indexNameFilter]) ||
+                        (itemLine[indexNameFilter]?.['id'] ?? 0) ===
+                        (filter[indexNameFilter]?.['id'] ?? -1) ||
+                        (itemLine[indexNameFilter]?.['codigo'] ?? 0) ===
+                        (filter[indexNameFilter]?.['codigo'] ?? -1)
+                      );
                     }
-                  });
-
-                  return dataFiltered.filter((d) => d === false).length === 0;
+                  }
                 });
-                this.datatableList$.next(filteredList);
-              }
-            }
-          } else {
-            if (typeof filter === 'string') {
-              this.textFilter = filter;
-            } else if (typeof filter === 'object') {
-              this.objectFilter = filter;
-            }
 
-            this.loadData();
+                return dataFiltered.filter((d) => d === false).length === 0;
+              });
+              this.datatableList$.next(filteredList);
+            }
+          }
+
+          switch (this.config?.typeDataList) {
+            case 'all':
+              if (this.datatableBackupList$.getValue().length === 0)
+                this.loadData();
+              break;
+            case 'onDemand':
+            case 'onScroll':
+              this.loadData();
+              break;
           }
         });
     }
